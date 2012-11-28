a37f6d5e4e86d7fcb124b5c53fcb6b5d91663843
maciel.bombonato <maciel.bombonato@gmail.com>
2012-11-28 21:24:15 -0200

    Retirada de arquivos de testes que não tem mais utilidade.

--- src/main/java/br/apolo/config/SecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package br.apolo.config;
-
-import org.springframework.context.annotation.*;
-
-@Configuration
-@ImportResource(value = "classpath:spring-security-context.xml")
-public class SecurityConfig {}
