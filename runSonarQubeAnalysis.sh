092b3448994f0247622ec386b0b2bdfe9a7445ce
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2017-10-23 19:42:39 -0200

    Update runSonarQubeAnalysis.sh

--- runSonarQubeAnalysis.sh
+++ runSonarQubeAnalysis.sh
@@ -7,8 +7,9 @@ export SONAR_TOKEN="5db4d3b663e7a6857fede685ec7dc4adcee636ac"
 
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
 	-Dsonar.host.url=$SONAR_HOST_URL \
-	-Dsonar.login=$SONAR_TOKEN \
 	-Dsonar.analysis.mode=preview \
 	-Dsonar.github.oauth=$GITHUB_TOKEN \
 	-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-	-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+	-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+	-Dsonar.login=macielbombonato@github \
+	-Dsonar.password=$SONAR_TOKEN
