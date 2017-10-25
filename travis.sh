52e51eef8fc4d1b57e37aae6a8ef1f3d578a6932
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2017-10-24 23:51:45 -0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,1 +1,10 @@
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
+
+export SONAR_HOST_URL="https://sonarcloud.io"
+export SONAR_ORGANIZATION="macielbombonato-github"
+export SONAR_TOKEN="94dab22c83c533a9ccd751e9646708e6d1117a00"
+
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
+	-Dsonar.host.url=$SONAR_HOST_URL \
+	-Dsonar.github.oauth=$GITHUB_TOKEN \
+	-Dsonar.login=$SONAR_ORGANIZATION \
+	-Dsonar.password=$SONAR_TOKEN
