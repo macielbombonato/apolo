83db8ca5c333307d5f51ad779183dea26bfd5866
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2017-10-25 00:21:03 -0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -4,13 +4,7 @@ export SONAR_ORGANIZATION="macielbombonato-github"
 export SONAR_PROJECT_KEY="apolo:apolo"
 export SONAR_TOKEN="94dab22c83c533a9ccd751e9646708e6d1117a00"
 
-export SONAR_EXCLUSIONS=" \
-/home/travis/build/macielbombonato/apolo/apolo-web/src/main/webapp/master/src/main/webapp/assets/bower_components, \
-/home/travis/build/macielbombonato/apolo/apolo-api/target, \
-/home/travis/build/macielbombonato/apolo/apolo-core/target, \
-/home/travis/build/macielbombonato/apolo/apolo-core-custom/target, \
-/home/travis/build/macielbombonato/apolo/apolo-web/target \
-"
+export SONAR_EXCLUSIONS="/home/travis/build/macielbombonato/apolo/apolo-web/src/main/webapp/master/src/main/webapp/assets/bower_components, /home/travis/build/macielbombonato/apolo/apolo-api/target, /home/travis/build/macielbombonato/apolo/apolo-core/target, /home/travis/build/macielbombonato/apolo/apolo-core-custom/target, /home/travis/build/macielbombonato/apolo/apolo-web/target "
 
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
 	-Dsonar.host.url=$SONAR_HOST_URL \
