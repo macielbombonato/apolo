c30add4f08db4017d14097fffcdc0e34cde31237
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2017-10-25 00:18:25 -0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -7,9 +7,9 @@ export SONAR_TOKEN="94dab22c83c533a9ccd751e9646708e6d1117a00"
 export SONAR_EXCLUSIONS= \
 " \
 /home/travis/build/macielbombonato/apolo/apolo-web/src/main/webapp/master/src/main/webapp/assets/bower_components, \
-/home/travis/build/macielbombonato/apolo/apolo-api/target \
-/home/travis/build/macielbombonato/apolo/apolo-core/target \
-/home/travis/build/macielbombonato/apolo/apolo-core-custom/target \
+/home/travis/build/macielbombonato/apolo/apolo-api/target, \
+/home/travis/build/macielbombonato/apolo/apolo-core/target, \
+/home/travis/build/macielbombonato/apolo/apolo-core-custom/target, \
 /home/travis/build/macielbombonato/apolo/apolo-web/target \
 "
 
